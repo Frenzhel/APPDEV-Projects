d2a4681b6504501db1729ca2dc00fda1fe3a8f0b
sagoco_f <sagoco_f@f112.lan>
2024-09-04 12:37:16 +0800

    Navbar

---
+++ my-first-react-app/src/Footer.jsx
@@ -0,0 +1,10 @@
+//this is my footer compomnent
+export default function Footer(){
+    return(
+        <footer>
+            <p>
+                &copy; {new DataTransfer.getFullYear()} My Food Website || Written by: Tom Hanks
+            </p>
+        </footer>
+    )
+}
