772bd414c1401e8ed4b7d0104ccf2c94615c3fec
sagoco_f <sagoco_f@f112.lan>
2024-09-25 12:20:34 +0800

    React Color Picker

---
+++ React-Color-Picker/src/App.jsx
@@ -0,0 +1,16 @@
+import Header from './Header'
+import ColorPicker from './ColorPicker'
+import Footer from './Footer'
+import './index.css'
+
+export default function App(){
+  return(
+    <>
+    <Header></Header>
+    <br />
+    <ColorPicker></ColorPicker>
+    <br />
+    <Footer></Footer>
+    </>
+  )
+}
---
+++ React-Color-Picker/src/Header.jsx
@@ -0,0 +1,9 @@
+export default function Header(){
+    //stateless component - no hooks, no functions, no states
+    return(
+        <header>
+            <h1>My Color Picker Project</h1>
+            <hr />
+        </header>
+    )
+}
