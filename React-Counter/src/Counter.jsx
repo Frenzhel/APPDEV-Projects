f62a96e68f677835ac2c34370503d2dea9823f89
sagoco_f <sagoco_f@f112.lan>
2024-09-27 11:32:28 +0800

    React-Counter

--- React-Counter/src/Counter.jsx
+++ React-Counter/src/Counter.jsx
@@ -17,6 +17,25 @@ export default function Counter(){
         //previous state => new state
         setCount(c => c = 0)
     }
+    const incrementCountFive = () =>{
+        setCount(c => c + 5)
+    }
+    const decrementCountFive = () =>{
+        setCount(c => c - 5)
+    }
+    function getRandomNum (){
+        let Number = count
+        if(Number>0){
+            Number = Math.floor(Math.random() * -999)
+        }else if(Number<0){
+            Number = Math.floor(Math.random() * 999)
+        }
+        return Number
+    }
+    const randomizeCount = () =>{
+        setCount(getRandomNum)
+            
+    }
     return(
         <>
         <div className="container">
@@ -27,6 +46,10 @@ export default function Counter(){
             <button className="button-container-decrement" onClick={decrementCount}>Decrement</button>
             <button className="button-container-reset" onClick={resetCount}>Reset</button>
             <button className="button-container-increment" onClick={incrementCount}>Increment</button>
+            <br />
+            <button className="button-container-decrement-five" onClick={decrementCountFive}>Decrement 5</button>
+            <button className="button-container-randomize" onClick={randomizeCount}>Randomize</button>
+            <button className="button-container-increment-five" onClick={incrementCountFive}>Increment 5</button>
         </div>
         </>
     )
