935740aad1a0de564c256a09eba2debc9ca59035
sagoco_f <sagoco_f@f112.lan>
2024-10-02 12:42:40 +0800

    10/02/24

---
+++ React-Updating-Arrays/src/App.jsx
@@ -0,0 +1,14 @@
+import Header from './Header'
+import FavoriteGames from './FavoriteGames'
+import Footer from './Footer'
+import './index.css'
+
+export default function App(){
+  return(
+    <>
+    <Header/>
+    <FavoriteGames/>
+    <Footer/>
+    </>
+  )
+}
