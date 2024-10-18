0ea4eb0ba2ace4c53c3f26b4a14669272095c046
sagoco_f <sagoco_f@f112.lan>
2024-10-18 13:11:53 +0800

    Shopping Cart

---
+++ React-Shopping-Cart/src/App.jsx
@@ -0,0 +1,11 @@
+import Header from './Header'
+import Footer from './Footer'
+import './App.css'
+import Login from './Login'
+export default function App() {
+  return(
+    <>
+      <Login />
+    </>
+  )
+}
---
+++ React-Shopping-Cart/src/Login.jsx
@@ -0,0 +1,67 @@
+import { useState } from "react"
+import Shop from "./Shop"
+import AdminPanel from "./AdminPanel"
+export default function Login() {
+    let items = [{id: 1, name: "Grapes", price: 50}, {id: 2, name: "Orange", price: 20}, {id: 3, name: "Pickles", price: 40}]
+    let accounts = [{id: 1, username: "customer", password: "customer", role: "customer"}, {id: 2, username: "admin", password: "admin", role: "admin"}]
+
+    const[isLoggedIn,setIsLoggedIn] = useState(false)
+
+    const[role, setRole] = useState("")
+
+    const[enteredUsernames, setEnteredUsernames] = useState("")
+    const[enteredPasswords, setEnteredPasswords] = useState("")
+    //function to get the username entered
+    function getUserName(event){
+        setEnteredUsernames(event.target.value)
+    }
+    //function to get the passwords entered
+    function getPassword(event){
+        setEnteredPasswords(event.target.value)
+    }
+    //function to handle login process
+    function handleLogin(){
+        accounts.map((account)=>{
+            if(account.username === enteredUsernames && account.password === enteredPasswords){
+                setIsLoggedIn(true)
+                setRole(account.role)
+            }
+        })
+    }
+
+    function renderLogin(){
+        return(
+            <div>
+                <h1>Login: </h1>
+                Username: <input type="text" placeholder="username" id="username" onChange={getUserName} />
+                <br />
+                Password: <input type="text" placeholder="password" id="password" onChange={getPassword} />
+                <br />
+                <button onClick={handleLogin}>Login</button>
+            </div>
+        )
+    }
+    //function to render the shop
+    function renderShop(){
+        return(
+            <Shop isLoggedIn = {isLoggedIn} items = {items} />
+        )
+    }
+    //function to render admin page
+    function renderAdmin(){
+        return(
+            <AdminPanel isLoggedIn = {isLoggedIn} accounts = {accounts}/>
+        )
+    }
+    //handles renderLogin based on login credentials
+    if(isLoggedIn === false){
+        return renderLogin()
+    }else{
+        if(role === "admin"){
+            return renderAdmin()
+        }
+        else{
+            return renderShop()
+        }
+    }
+}
