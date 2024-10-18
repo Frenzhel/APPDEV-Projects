3d67cbbfaf08e4b4d154889b1b89adb9748fcab8
sagoco_f <sagoco_f@f112.lan>
2024-10-18 11:13:10 +0800

    PokeDex

--- React-PokeDex/src/Footer.jsx
+++ React-PokeDex/src/Footer.jsx
@@ -2,8 +2,7 @@ export default function Footer(){
     return(
         <>
         <footer>
-            <hr />
-            <p>&copy; {new Date().getFullYear()} My Favorite Games Website || Written by: Frenzhel (Gourmand) Vhonn Sagoco</p>
+            <p>&copy; {new Date().getFullYear()} PokeDex Website || Written by: James Peter Gonzaga & Frenzhel (Zeee) Vhonn Sagoco</p>
         </footer>
         </>
     )
--- React-PokeDex/src/Header.jsx
+++
@@ -1,8 +0,0 @@
-export default function Header(){
-    return(
-        <header>
-            <h1>This Is My List Of Favorite Games</h1>
-            <hr />
-        </header>
-    )
-}
